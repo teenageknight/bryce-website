2f36a250a190ea012e89f15df621145b05191579
Bryce Jackson <bkajackson9@gmail.com>
2023-06-03 11:44:32 -0400

    Inital Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Bryce Website Landing</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
