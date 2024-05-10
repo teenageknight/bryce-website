6617ab35dadfeeb26dde3cb5890dcd801cd9c16e
Bryce Jackson <bkajackson9@gmail.com>
2024-05-10 12:30:27 -0400

    deleted key

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -111,7 +111,7 @@ export const getCensusDataQuery = onCall({ timeoutSeconds: 120 }, async request 
 
     let params =
         "get=NAME,B01001_001E,B01001_003E,B01001_027E,B01001_004E,B01001_005E,B01001_006E,B01001_028E,B01001_029E,B01001_030E,B01001_020E,B01001_021E,B01001_022E,B01001_023E,B01001_024E,B01001_025E,B01001_044E,B01001_045E,B01001_046E,B01001_047E,B01001_048E,B01001_049E,B11001_001E,B17017_002E,B19013_001E,B23025_002E,B23025_005E,B01002_001E,B03003_003E,B02001_003E,B02001_002E,B02001_005E,B02001_004E,B02001_008E";
-    let key = "&key=a9377975bb59e3a5a904e4dc7282aba021c9c55e";
+    // let key = "&key=a9377975bb59e3a5a904e4dc7282aba021c9c55e";
     for (const row of table) {
         let query =
             "&for=block%20group:" +
@@ -123,7 +123,7 @@ export const getCensusDataQuery = onCall({ timeoutSeconds: 120 }, async request 
             "%20tract:" +
             row.tract_code;
 
-        let url = "https://api.census.gov/data/2020/acs/acs5?" + params + "&get=" + query + key;
+        let url = "https://api.census.gov/data/2020/acs/acs5?" + params + "&get=" + query;
 
         console.log("url", url);
 
