e543e500f175e5dbf57447b464115a6648d654e0
Bryce Jackson <bkajackson9@gmail.com>
2024-05-19 13:20:05 -0400

    updates to documentation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,15 @@
+import { getCensusDataQuery } from "../services/functions";
 export function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
+            <button
+                onClick={() => {
+                    console.log("test");
+                    getCensusDataQuery();
+                }}>
+                test
+            </button>
         </div>
     );
 }
