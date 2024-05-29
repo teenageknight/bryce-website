ed1b32c0db1e90dd37a0ef8e2e6c6345165a4b82
Bryce Jackson <bkajackson9@gmail.com>
2024-05-28 21:23:49 -0400

    Added data project page

---
+++ src/pages/FWADataProject.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function FWADataProjectPage() {
+    return (
+        <>
+            <p>Upload map spreadsheets</p>
+            {/* Preformat for now and write that code later */}
+            <p>Button to upload unformatted excel</p>
+            {/* On button press, format the spreadsheet, and geocode results to get api repsonse */}
+            {/* Generate a formatted map */}
+            {/* Output the embedded iframe code */}
+        </>
+    );
+}
