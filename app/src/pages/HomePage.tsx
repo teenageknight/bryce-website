e0938b3178c130803591e6c276f76cf114acd73e
teenageknight <bkajackson9@gmail.com>
2024-07-29 18:14:35 -0400

    update homepage

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { BACKGROUND_COLOR } from "../styles/consts";
 import Markdown from "react-markdown";
 import aboutme from "../assets/markdown/aboutme.md?raw";
 import ProfilePic from "../assets/images/profile.jpeg";
+import { PortfolioCard } from "../components/portfolio-card/portfolio-card";
 
 // TODO: The hard coded colors on this page should be switched to dynamic, they are set to this hard coded cause tailwind
 //       cannot render dynamic colors.
 // TODO: Dynamically pull this information from Github using a token
 export function HomePage() {
+    const navigate = useNavigate();
+
     return (
         <div className={`h-full`} style={{ backgroundColor: `${BACKGROUND_COLOR}` }}>
             <div className="m-4 pb-5">
@@ -18,22 +22,37 @@ export function HomePage() {
                 </p>
             </div>
             <div className="grid grid-cols-3 m-4">
-                <div className="grid">
-                    <img src={ProfilePic} className="h-72 rounded-full border-2" style={{ borderColor: "#30363D" }} />
+                <div className="flex flex-col col-span-1">
+                    <img
+                        src={ProfilePic}
+                        className="h-2/3 rounded-full border-2 w-fit"
+                        style={{ borderColor: "#30363D" }}
+                    />
                     <div>
                         <p className="text-2xl font-bold pt-4">Bryce Jackson</p>
                         <p className="text-xl text-[#8D96A0]">teenageknight</p>
                         <p className="font-semibold mt-3">Associate Consultant @CapTech</p>
                     </div>
                 </div>
-                <div className="grid col-span-2">
+                <div className="flex-col col-span-2 flex">
                     <div className="border-2 rounded p-4 h-fit" style={{ borderColor: "#30363D" }}>
                         <a className={"font-semibold text-sm"} href="https://github.com/teenageknight/teenageknight">
                             teenageknight / README.md
                         </a>
                         <Markdown className="markdown">{aboutme}</Markdown>
                     </div>
-                    <p>Portfolio</p>
+                    <div onClick={() => {}} className="h-max mt-3">
+                        <p className="font-semibold text-lg my-2">Portfolio</p>
+                        {/* TODO: Create a component out of this */}
+
+                        <PortfolioCard
+                            onClick={() => {
+                                navigate("/fwa-census-calculator");
+                            }}
+                            title={"FWA Data Project"}
+                            description="This is a collection of tooling designed to assist Food Well Allience better their understanding of data"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
