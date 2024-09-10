70c726b41328e4b90fcdf766d3d2c0d6c247a641
teenageknight <bkajackson9@gmail.com>
2024-09-10 08:08:24 -0400

    this needs to build

--- functions/src/test.ts
+++ functions/src/test.ts
@@ -1,35 +1,35 @@
-const fs = require("fs");
-const { parse } = require("csv-parse");
+// const fs = require("fs");
+// const { parse } = require("csv-parse");
 
-let data = [];
+// let data = [];
 
-async function parseFile(filePath) {
-    let x = fs
-        .createReadStream("./cjest.csv")
-        .pipe(
-            parse({
-                delimiter: ",",
-                columns: true,
-                ltrim: true,
-            })
-        )
-        .on("data", function (row) {
-            // 👇 push the object row into the array
-            data.push(row);
-        })
-        .on("error", function (error) {
-            console.log(error.message);
-        })
-        .on("end", function () {
-            // 👇 log the result array
-            console.log("parsed csv data:");
-            console.log(data);
-        });
-    return x;
-}
+// async function parseFile(filePath) {
+//     let x = fs
+//         .createReadStream("./cjest.csv")
+//         .pipe(
+//             parse({
+//                 delimiter: ",",
+//                 columns: true,
+//                 ltrim: true,
+//             })
+//         )
+//         .on("data", function (row) {
+//             // 👇 push the object row into the array
+//             data.push(row);
+//         })
+//         .on("error", function (error) {
+//             console.log(error.message);
+//         })
+//         .on("end", function () {
+//             // 👇 log the result array
+//             console.log("parsed csv data:");
+//             console.log(data);
+//         });
+//     return x;
+// }
 
-parseFile("./cjest.csv").then(d => {
-    console.log("done");
-    // console.log(d);
-});
-console.log("Finished");
+// parseFile("./cjest.csv").then(d => {
+//     console.log("done");
+//     // console.log(d);
+// });
+// console.log("Finished");
